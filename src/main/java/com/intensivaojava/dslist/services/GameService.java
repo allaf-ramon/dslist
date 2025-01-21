4600d5e26209320925075992954e1108aeffcf53
Allaf Ramon <allafr1999@gmail.com>
2025-01-21 14:45:14 -0300

    GameMinProjection

--- src/main/java/com/intensivaojava/dslist/services/GameService.java
+++ src/main/java/com/intensivaojava/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.intensivaojava.dslist.services;
 import com.intensivaojava.dslist.dto.GameDTO;
 import com.intensivaojava.dslist.dto.GameMinDTO;
 import com.intensivaojava.dslist.entities.Game;
+import com.intensivaojava.dslist.projections.GameMinProjection;
 import com.intensivaojava.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
-        return result.stream().map(x -> new GameMinDTO(x)).toList();
+        return result.stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(GameMinDTO::new).toList();
     }
 }
