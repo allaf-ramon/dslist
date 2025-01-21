dbc6f8c2db85ee3023a9a21d51f2a6b32f36b999
Allaf Ramon <allafr1999@gmail.com>
2025-01-21 13:41:00 -0300

    GET lists

--- src/main/java/com/intensivaojava/dslist/controllers/GameController.java
+++ src/main/java/com/intensivaojava/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.intensivaojava.dslist.controllers;
 
+import com.intensivaojava.dslist.dto.GameDTO;
 import com.intensivaojava.dslist.dto.GameMinDTO;
 import com.intensivaojava.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +18,13 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
+    }
+
     @GetMapping
     public List<GameMinDTO> findAll() {
-        List<GameMinDTO> result = gameService.findAll();
-        return result;
+        return gameService.findAll();
     }
 }
